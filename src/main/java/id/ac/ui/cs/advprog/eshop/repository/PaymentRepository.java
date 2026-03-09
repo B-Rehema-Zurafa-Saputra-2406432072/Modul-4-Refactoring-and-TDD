c16f28eccb3eaf3b785e6948c2b64cb8a781ea25
Rehema Zurafa Saputra <rehemazurafasaputra@gmail.com>
2026-03-09 13:22:24 +0700

    [GREEN] Implement PaymentRepository logic

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -11,14 +11,29 @@ public class PaymentRepository {
     private List<Payment> paymentData = new ArrayList<>();
 
     public Payment save(Payment payment) {
-        return null; // Skeleton return
+        for (int i = 0; i < paymentData.size(); i++) {
+            Payment savedPayment = paymentData.get(i);
+            if (savedPayment.getId().equals(payment.getId())) {
+                // kalau payment exists, update
+                paymentData.set(i, payment);
+                return payment;
+            }
+        }
+        // kalau gak ada, new
+        paymentData.add(payment);
+        return payment;
     }
 
     public Payment findById(String id) {
-        return null; // Skeleton return
+        for (Payment savedPayment : paymentData) {
+            if (savedPayment.getId().equals(id)) {
+                return savedPayment;
+            }
+        }
+        return null;
     }
 
     public List<Payment> getAllPayments() {
-        return null; // Skeleton return
+        return paymentData;
     }
 }
